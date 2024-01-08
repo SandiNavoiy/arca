b44912bd9f4a70d4138979cdd7bed4037d30b114
Alex <santes.au@gmail.com>
2024-01-08 10:38:41 +0300

    1+

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import requests
+import datetime
+import json
 
-respose  = requests.get("https://arsagera.ru/api/v1/funds/fa/fund-metrics/?total_net_assets")
-print(respose.json())
+import requests
+dt_now = datetime.date.today()
+#вывод информации о стоимости пая
+respose  = requests.get("https://arsagera.ru/api/v1/funds/fa/fund-metrics/?from=2023-12-27&to=2023-12-31")
+if respose.status_code == 200:
+    # Преобразование JSON в словарь
+    data = respose.json()
+
+for i in data['data']:
+    print(i['nav_per_share'])
+print(data['data'])
+
+
+#вывод текущей
+respose_now = requests.get(f"https://arsagera.ru/api/v1/funds/fa/fund-metrics/?date={dt_now}")
+print(respose_now.json())
