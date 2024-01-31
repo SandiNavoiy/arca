8abe49ce51220cd060e2519fec26189e47e960f0
Sandi_Navoiy <santes.au@gmail.com>
2024-01-31 18:52:54 +0300

    2

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import datetime
 import matplotlib.pyplot as plt
 
 import requests
+#Текущая дата
 dt_now = datetime.date.today()
+#Задаем диапазон дат
+date1 = datetime.date(2023, 12, 27)
+date2 = datetime.date(2023, 12, 30)
 
 #вывод информации о стоимости пая
-respose  = requests.get("https://arsagera.ru/api/v1/funds/fa/fund-metrics/?from=2023-12-27&to=2023-12-31")
+respose  = requests.get(f"https://arsagera.ru/api/v1/funds/fa/fund-metrics/?from={date1}&to={date2}")
 if respose.status_code == 200:
     # Преобразование JSON в словарь
     data = respose.json()
 x = []
 y = []
-index = 0
+index = date1
 for i in data['data']:
     x.append(index)
-    index += 1
+    index += datetime.timedelta(days=1)
     y.append(i['nav_per_share'])
 # print(data['data'])
 #Вывод графического представления об активах фонда
