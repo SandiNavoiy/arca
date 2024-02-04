77a20da0480d84d4687afd7199212d496f506bae
Alex <santes.au@gmail.com>
2024-02-04 17:39:22 +0300

    Добавлен вывод в виде графика, свм вывод убран в функцию

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import requests
 #Текущая дата
 dt_now = datetime.date.today()
 #Задаем диапазон дат
-date1 = datetime.date(2023, 12, 27)
+date1 = datetime.date(2023, 11, 27)
 date2 = datetime.date(2023, 12, 30)
+
+
 
 #вывод информации о стоимости пая
 respose  = requests.get(f"https://arsagera.ru/api/v1/funds/fa/fund-metrics/?from={date1}&to={date2}")
@@ -22,15 +24,24 @@ for i in data['data']:
 
     y.append(i['nav_per_share'])
 
-# print(data['data'])
+
 #Вывод графического представления об активах фонда
-plt.plot(x,y)
-plt.title("Активы фонда")
-plt.xlabel("Дни")
-plt.ylabel("Сумма")
-plt.show()
+def grafic(os_x,os_y):
+    """Вывод графической части"""
+    plt.plot(os_x,os_y)
+    plt.title("Активы фонда")
+    plt.xlabel("Дни")
+    plt.ylabel("Сумма")
+    if len(os_x)<10:
+        k = 1
+    elif 10 <= len(os_x) <= 100:
+        k = 20
+    elif len(os_x) > 100:
+        k = 50
+    plt.xticks(os_x[::k], rotation=45)
+    plt.show()
 
-
+grafic(x,y)
 #вывод текущей
 print("вывод текущей")
 respose_now = requests.get(f"https://arsagera.ru/api/v1/funds/fa/fund-metrics/?date={dt_now}")
