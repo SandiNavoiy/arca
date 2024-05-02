dc316631e5bc0cdbd00641d02e47c117d1e959ca
Alex <santes.au@gmail.com>
2024-05-02 14:47:53 +0300

    +

--- 2.py
+++ 2.py
@@ -7,7 +7,7 @@ with open('1.txt', 'r', encoding="utf-8") as file:
 
 rez = []
 for i in new:
-    if  "з" not in i and "е" not in i and "б" not in i and "р" not in i   and "ш" not in i  and "ю" not in i and "х" not in i and "ф" not in i  and "ь" not in i and "г" not in i and "в" not in i and  i[4] == "а" and  i[1] == "л" and  i[2] != "а" and  i[2] == "и" and "э" not in i and "т" not in i:
+    if  "з" not in i and "е" not in i and "б" not in i and "р" not in i  and "а" not in i and "с" not in i and "д" not in i and "н" not in i and "ч" not in i and i[1] == "у" and i[3] == "о" and "п" in i and "к" in i:
         rez.append(i)
 
 print(rez)
