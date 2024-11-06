c0c056a21cb877ce40f85b700f10be8c5cdd63a2
Alex <santes.au@gmail.com>
2024-11-06 10:19:26 +0300

    +

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 with open('1.txt', 'r', encoding="utf-8") as file:
     new = [line.strip() for line in file if len(line.strip()) == 5]  # Убираем символ перевода строки сразу и фильтруем длину строки
 
-exclude_chars = set("бувтолпжрцд")
-rez = [i for i in new if not exclude_chars.intersection(i) and i[4] == "а" and i[2] == "и" and i[1] == "н"]
+exclude_chars = set("букапюнрдшме")
+rez = [i for i in new if not exclude_chars.intersection(i) and i[3] == "в"]
 
 print(rez)
