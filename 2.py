ae2dd97ab83a22ad857a3ce9c919a5e34904b4be
Sandi_Navoiy <santes.au@gmail.com>
2024-11-10 10:58:13 +0300

    для тинькова

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 with open('1.txt', 'r', encoding="utf-8") as file:
     new = [line.strip() for line in file if len(line.strip()) == 5]  # Убираем символ перевода строки сразу и фильтруем длину строки
 
-exclude_chars = set("букгот")
-rez = [i for i in new if not exclude_chars.intersection(i) and i[4] == 'н' and i[1] == 'и'and i[3] == 'а']
+exclude_chars = set("буквслинзй")
+rez = [i for i in new if not exclude_chars.intersection(i) and i[1] == "а" and i[3] == "о" and i[0] != "м" and i[4] != "р" and i[1] != "п" and i[4] != "а"]
 
 print(rez)
