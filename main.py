54d3230e504edfe8a235b820938bf2f3a9505926
Sandi_Navoiy <santes.au@gmail.com>
2025-03-13 16:42:35 +0300

    Готовый код

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import datetime
 import requests
-from unit import grafic
+from matplotlib import pyplot as plt
+
 
 #Текущая дата
 dt_now = datetime.date.today()
-#Задаем диапазон дат
-date1 = datetime.date(2023, 11, 27)
-date2 = datetime.date(2023, 12, 30)
+#Задаем диапазон дат один год
+dt_year = datetime.date.today() - datetime.timedelta(days=365)
 
+date1 = dt_year
+date2 = dt_now
 
+print(" Выберите фонд")
+print("fa : Арсагера — фонд акций")
+print("f4si :Арсагера — фонд смешанных инвестиций")
+print("f64: Арсагера — акции 6.4")
+print("fo : Арсагера – фонд облигаций КР 1.55")
+print("Введите из представленных вариантов")
+fond = input()
 
 #вывод информации о стоимости пая
-respose  = requests.get(f"https://arsagera.ru/api/v1/funds/fa/fund-metrics/?from={date1}&to={date2}")
+respose  = requests.get(f"https://arsagera.ru/api/v1/funds/{fond}/fund-metrics/?from={date1}&to={date2}")
 if respose.status_code == 200:
     # Преобразование JSON в словарь
     data = respose.json()
@@ -20,15 +29,23 @@ y = []
 index = date1
 for i in data['data']:
     x.append(str(index))
-    index += datetime.timedelta(days=1)
+    index += datetime.timedelta(days=30)
 
     y.append(i['nav_per_share'])
 
-
+d = {"fa" : "Арсагера — фонд акций","f4si": "Арсагера — фонд смешанных инвестиций", "f64": "Арсагера — акции 6.4", "fo" : "Арсагера – фонд облигаций КР 1.55" }
 #Вывод графического представления об активах фонда
+# Создание графика
+podpis = d[fond]
+plt.plot(x, y, label="График курса")
 
-grafic(x,y)
-#вывод текущей
-print("вывод текущей")
-respose_now = requests.get(f"https://arsagera.ru/api/v1/funds/fa/fund-metrics/?date={dt_now}")
-print(respose_now.json())
+# Добавление подписей
+plt.title(podpis)
+plt.xlabel("Ось дат")
+plt.ylabel("Ось значений, руб")
+
+# Добавление легенды
+plt.legend()
+
+# Отображение графика
+plt.show()
--- stoimosti_pai.py
+++ stoimosti_pai.py
@@ -1,11 +1,14 @@
 from datetime import date, timedelta
-
 import requests
-
+# Текущая дата - 2 дня назад
 dt_now = date.today() - timedelta(days=2)
-print(dt_now)
-# fa : "Арсагера — фонд акций" f4si : "Арсагера — фонд смешанных инвестиций" f64 : "Арсагера — акции 6.4" fo : "Арсагера – фонд облигаций КР 1.55"
-# nav_per_share : Стоимость пая. Тип: float.
-respose_now = requests.get(f"https://arsagera.ru/api/v1/funds/fa/fund-metrics/?date={dt_now}")
-
-print(respose_now.json())
+print(f"Сегодняшняя дата {date.today()}")
+# Список фордов
+d = {"fa" : "Арсагера — фонд акций","f4si": "Арсагера — фонд смешанных инвестиций", "f64": "Арсагера — акции 6.4", "fo" : "Арсагера – фонд облигаций КР 1.55" }
+#Перебор
+for k,v in d.items():
+    respose_now = requests.get(f"https://arsagera.ru/api/v1/funds/{k}/fund-metrics/?date={dt_now}")
+    if respose_now.status_code == 200:
+        print(f"{v}: -  {respose_now.json()['data'][0]['nav_per_share']} рублей, копеек")
+    else:
+        print(f"{v}: - Нет данных по текущему дню")
